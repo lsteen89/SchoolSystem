12ec0e7c1e393cf5421e1b81549a8a90a9d7dbc1
linusmller <linus.mller@gmail.com>
2023-10-21 13:57:31 +0200

    Fixed Loginmanager and a Usersession class that keeps track of the current logged in user

---
+++ SchoolSystemLibary/Login/LoginManager.cs
@@ -0,0 +1,57 @@
+﻿using Dapper;
+using SchoolSystemLibary.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SchoolSystemLibary.Login
+{
+    public class LoginManager
+    {
+        /// <summary>
+        /// This manages the Login check.
+        /// </summary>
+        /// <param name="username">The username supplied in the form</param>
+        /// <param name="password">The password supplied in the form</param>
+        /// <returns>userModel if true, Null otherwise</returns>
+        public UserModel AuthenticateUser(string username, string password)
+        {
+
+            using (var connection = GlobalConfig.GetConnection())
+            {
+                var user = connection.QuerySingleOrDefault<UserModel>("SELECT * FROM Users WHERE LoginName = @LoginName", new { LoginName = username });
+                if (user != null)
+                {
+                    bool PasswordValid = true; //Todo Fix password verification
+                    if (PasswordValid)
+                    {
+                        UserModel UserModel = new UserModel
+                        {
+                            LoginName = user.LoginName,
+                            Role = user.Role,
+                            Persoid = user.Persoid,
+                        };
+                        return user;
+                    }
+                }
+                return null;
+            }
+        }
+        private SecureString CreateSecureString(string value)
+        {
+            SecureString secureString = new SecureString();
+            foreach (char c in value)
+            {
+                secureString.AppendChar(c);
+            }
+            secureString.MakeReadOnly();
+            return secureString;
+        }
+    }
+}
--- SchoolSystemUI/SchoolSystemMain.Designer.cs
+++ SchoolSystemUI/SchoolSystemMain.Designer.cs
@@ -29,7 +29,18 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SchoolSystemMain));
+            this.MainWelcomeLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // MainWelcomeLabel
+            // 
+            this.MainWelcomeLabel.AutoSize = true;
+            this.MainWelcomeLabel.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.MainWelcomeLabel.Location = new System.Drawing.Point(44, 67);
+            this.MainWelcomeLabel.Name = "MainWelcomeLabel";
+            this.MainWelcomeLabel.Size = new System.Drawing.Size(101, 25);
+            this.MainWelcomeLabel.TabIndex = 0;
+            this.MainWelcomeLabel.Text = "Welcome";
             // 
             // SchoolSystemMain
             // 
@@ -37,16 +48,21 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1371, 750);
+            this.Controls.Add(this.MainWelcomeLabel);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.Color.DeepSkyBlue;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "SchoolSystemMain";
             this.Text = "School system ©";
             this.Load += new System.EventHandler(this.SchoolSystemMain_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Label MainWelcomeLabel;
     }
 }
